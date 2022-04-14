d2a4a549b22e5854706683e51643f565d93ada3d
chamberezigbo <chamberezigbo@gmail.com>
2022-04-14 12:55:08 +0100

    almost done remaning footer

---
+++ index.php
@@ -0,0 +1,338 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="res/css/main.style.css?v=8">
+     <link rel="stylesheet" href="res/css/global.style.css?v=8">
+
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <link rel="stylesheet" href="res/fontawesome-free-6.0.0-web/css/all.css">
+
+     <title>Hello, world!</title>
+</head>
+
+<body>
+     <div class="container-fluid ">
+          <nav class="navbar navbar-expand-lg navbar-dark fixed-top first-nav">
+               <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                         <img src="res/image//Screenshot_7.png" alt="" width="150" height="35">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                         <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                         <ul class="navbar-nav">
+                              <li class="nav-item">
+                                   <a class="nav-link active" aria-current="page" href="#">Home</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link" href="#">Features</a>
+                              </li>
+                              <li class="nav-item">
+                                   <a class="nav-link" href="#">Pricing</a>
+                              </li>
+                         </ul>
+                    </div>
+               </div>
+          </nav>
+
+     </div>
+
+     <div class="tracking-sec">
+          <div class="container">
+               <div class="container mx-auto w-50 pt-5">
+                    <div class="pt-5">
+                         <h3 class="text-capitalize">track your shipment</h3>
+                    </div>
+                    <div class="input-group mb-2 text-center  ">
+                         <input type="text" class="form-control" placeholder="Enter your tracking number(s)" aria-label="Recipient's username" aria-describedby="button-addon2">
+                         <button class="btn btn-danger" type="button" id="button-addon2">Button</button>
+                    </div>
+                    <div class="">
+                         <p>DHL Express Reference Tracking></p>
+                    </div>
+
+               </div>
+          </div>
+     </div>
+
+     <!-- Todo: finch up with the cards on hover effect  -->
+     <div class="container mt-5">
+          <div class="container">
+               <div class="row">
+                    <div class="col-lg-3">
+                         <div class="card text-dark mb-3 card-inquiry bg-white courser-pointer">
+                              <div class="card-body text-center ">
+                                   <h6 class="card-text">
+                                        <i class="fa-solid fa-calendar-days text-danger fa-2x"></i>
+                                   </h6>
+                                   <h6 class="card-text hoverDesc">
+                                        Ship Now
+                                   </h6>
+                                   <p class="card-text fwt-sm fw-lighter">
+                                        find the right service
+                                   </p>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                         <div class="card text-dark mb-3 card-inquiry bg-white courser-pointer">
+                              <div class="card-body text-center ">
+                                   <h6 class="card-text">
+                                        <i class="fa-solid fa-square-check text-danger fa-2x"></i>
+                                   </h6>
+                                   <h6 class="card-text hoverDesc">
+                                        Get A Quote
+                                   </h6>
+                                   <p class="card-text fwt-sm fw-light">
+                                        Estimate cost to share and compare
+                                   </p>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                         <div class="card text-dark mb-3 card-inquiry bg-white card-for-buz courser-pointer">
+                              <div class="card-body text-center ">
+                                   <h6 class="card-text">
+                                        <i class="fa-solid fa-city fa-2x text-danger"></i>
+                                   </h6>
+                                   <h6 class="card-text hoverDesc">
+                                        DHL For Business
+                                   </h6>
+                                   <p class="card-text fwt-sm fw-light">
+                                        Shipping regularly? Request a business account and profit from
+                                        from exclusive benefits
+                                   </p>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                         <div class="card text-dark mb-3 card-inquiry bg-white courser-pointer">
+                              <div class="card-body text-center ">
+                                   <h6 class="card-text">
+                                        <i class="fa-solid fa-user text-danger fa-2x"></i>
+                                   </h6>
+                                   <h6 class="card-text hoverDesc">
+                                        Portal Login
+                                   </h6>
+                                   <p class="card-text fwt-sm fw-light">
+                                        Our divisions offer different online tools for your shipping and logistics needs
+                                   </p>
+                              </div>
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </div>
+
+     <div class="container text-danger covid-sec p-2 mt-2">
+          <h6><strong> Covid-19 Updates > </strong></h6>
+     </div>
+
+
+     <section>
+          <div class="container my-5 ">
+               <div class="text-center mb-5">
+                    <h2><strong>Shipping Service</strong></h2>
+               </div>
+               <div class="container">
+                    <div class="row">
+                         <div class="col-lg-3">
+                              <div class="card text-center">
+                                   <div class="card-body">
+                                        <h2 class="card-title"><i class="fa-solid fa-envelopes-bulk text-danger"></i></h2>
+                                        <p class="card-text">
+                                             <strong>Document & Parcel</strong>
+                                        </p>
+                                        <p class="card-text card-text-hover">
+                                             Domestic and <br> international <span class="text-danger"> ></span>
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="col-lg-2">
+                              <div class="card text-center">
+                                   <div class="card-body">
+                                        <h2 class="card-title"><i class="fa-solid fa-plane-departure text-danger"></i></h2>
+                                        <p class="card-text">
+                                             <strong>Air Freight</strong>
+                                        </p>
+                                        <p class="card-text card-text-hover">
+                                             international <br> Same Day <span class="text-danger"> ></span>
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="col-lg-2">
+                              <div class="card text-center">
+                                   <div class="card-body">
+                                        <h2 class="card-title"><i class="fa-solid fa-ship text-danger"></i></h2>
+                                        <p class="card-text">
+                                             <strong>Ocean Freight</strong>
+                                        </p>
+                                        <p class="card-text card-text-hover">
+                                             International <span class="text-danger"> ></span>
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="col-lg-2">
+                              <div class="card text-center">
+                                   <div class="card-body">
+                                        <h2 class="card-title"><i class="fa-solid fa-truck-moving text-danger"></i></h2>
+                                        <p class="card-text">
+                                             <strong>Road Freight</strong>
+                                        </p>
+                                        <p class="card-text card-text-hover">
+                                             International <span class="text-danger"> ></span>
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="col-lg-3">
+                              <div class="card text-center">
+                                   <div class="card-body">
+                                        <h2 class="card-title"><i class="fa-solid fa-plane-arrival text-danger"></i></h2>
+                                        <p class="card-text">
+                                             <strong>Rail Freight</strong>
+                                        </p>
+                                        <p class="card-text card-text-hover">
+                                             Between Europe And <br> Asia <span class="text-danger"> ></span>
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+
+                    </div>
+               </div>
+     </section>
+
+     <section>
+          <div class="container my-5">
+               <div class="text-center mb-5">
+                    <h2><strong>Logistics Solutions</strong></h2>
+               </div>
+
+               <div class="container">
+                    <div class="row">
+                         <div class="col-lg-4 pt-2">
+                              <div class="card card-shadow">
+                                   <img src="res/image/packages.jpg" class="card-img-top w-50" alt="...">
+                                   <div class="card-body">
+                                        <h5 class="card-title">Warehouse Solutions</h5>
+                                        <p class="card-text text-danger fw-light">
+                                             Warehouse for Small and Medium Business ><br><br>
+                                             Warehouse for Enterprise >
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="col-lg-4 pt-2">
+                              <div class="card card-shadow">
+                                   <img src="res/image/plan-mood.jpg" class="card-img-top" alt="...">
+                                   <div class="card-body">
+                                        <h5 class="card-title">Transport and Distribution</h5>
+                                        <p class="card-text text-danger fw-light">
+                                             Warehouse for Small and Medium Business ><br>
+                                             Warehouse for Enterprise >
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="col-lg-4 pt-2">
+                              <div class="card card-shadow">
+                                   <img src="res/image/man-contract.jpg" class="card-img-top" alt="...">
+                                   <div class="card-body">
+                                        <h5 class="card-title">Contract Logistics</h5>
+                                        <p class="card-text text-danger fw-light">
+                                             Warehouse for Small and Medium Business >
+                                        </p>
+                                   </div>
+                              </div>
+                         </div>
+
+                    </div>
+               </div>
+
+               <div class="container text-center my-5">
+                    <h1 class="text-danger"><strong>IMPORTANT INFORMATION</strong></h1>
+                    <h6 class="my-5"> <strong>Service bulletins keep you up to date with events that might affect our high service standards</strong></h6>
+                    <p class="text-danger">SMS-Scam “Download Our Application to Track Your Parcel”></p>
+                    <hr>
+               </div>
+
+               <div class="container green-logistics my-5">
+                    <div>
+                         <div class="row">
+                              <div class="col-lg-6 side-sect pt-3">
+                                   <div class="container-fluid">
+                                        <h3>
+                                             <strong>Green Logistics</strong>
+                                        </h3>
+                                        <p>
+                                             By the year 2050 we want to reduce all logistics-related emissions to zero – a highly ambitious goal. Our mission as the leading mail
+                                             and logistics company is to connect people and improve lives.
+                                        </p>
+                                        <button class="btn btn-danger">Learn More</button>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
+               <div class="container vision-logistics my-5">
+                    <div>
+                         <div class="row">
+                              <div class="col-lg-6 side-sect pt-3 ms-auto">
+                                   <div class="container-fluid">
+                                        <h3>
+                                             <strong>Our Divisions</strong>
+                                        </h3>
+                                        <p>
+                                             DHL connects people in over 220 countries and territories worldwide. Driven by the power of more than 400,000 employees, we deliver integrated services and
+                                             tailored solutions for managing and transporting letters, goods and information.
+                                        </p>
+                                        <button class="btn btn-danger">Learn More</button>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
+               <div class="container">
+                    <div class="row">
+                         <div class="col-lg-3">
+                              <div>
+                                   <h5 class="text-danger"><strong>Help Center</strong></h5> <br>
+                                   <p class="card-text-hover"> Contact US</p>
+                                   <p class="card-text-hover">Customer Portal Logins </p>
+                                   <p class="card-text-hover">Digital Partners and Integration </p>
+                                   <p class="card-text-hover">Developers Portal </p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </section>
+
+
+
+     <!-- Optional JavaScript; choose one of the two! -->
+
+     <?php
+     require_once('footer.php');
+     ?>
